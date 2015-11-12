b14d3f42bd4792352e14211c8fee253e0114e1fa
Xiaoyu Yuan <rain1994715@Gatech.edu>
2015-11-12 14:27:37 -0500

    adding questions for N body
    with some changes on website

---
+++ httpdocs/js/questions/questionsforN.js
@@ -0,0 +1,41 @@
+$(document).ready(function() 
+{    $("#results").click(function() {                
+
+if (!$("input[@name=q1]:checked").val() ||            
+!$("input[@name=q2]:checked").val() ||            
+!$("input[@name=q3]:checked").val() 
+) {            
+alert("Oops! There are questions not yet done!");        
+}        
+
+else {            
+var cat1name = "1";            
+var cat2name = "2";            
+var cat3name = "3";                                 
+var cat11name = "None";            
+            
+
+var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
+           
+var cat2 = ($("input[@name=q2]:checked").val() != "a");  
+
+var cat3 = ($("input[@name=q3]:checked").val() != "b");  
+
+var cat11 = (!cat1 && !cat2 && !cat3); var categories = [];                        
+
+if (cat1) { categories.push(cat1name) };            
+if (cat2) { categories.push(cat2name) };            
+if (cat3) { categories.push(cat3name) };                                       
+if (cat11) { categories.push(cat11name) };                        
+
+var catStr = 'You answered the following questions incorrectly: ' + categories.join(', ') + '';                     
+$("#categorylist").text(catStr);                        
+$("#categorylist").show("slow");            
+
+if (cat1) { $("#category1").show("slow"); };            
+if (cat2) { $("#category2").show("slow"); };            
+if (cat3) { $("#category3").show("slow"); };       
+if (cat11) { $("#category11").show("slow"); };
+{ $("#closing").show("slow"); };
+}
+    });});
